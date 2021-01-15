61ee2a5565bf182853086cc278140455b1895ca8
Syahin Syah <syahinsyah7@gmail.com>
2021-01-16 02:37:53 +0800

    done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if(this.scrollY > 10)
+        if(this.scrollY > 20)
         {
             $('.navbar').addClass("sticky");
         }
@@ -8,15 +8,65 @@ $(document).ready(function(){
         {
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500)
+        {
+            $('.scroll-up-button').addClass("show");
+        }
+        else
+        {
+            $('.scroll-up-button').removeClass("show");
+        }
         });
+
+        // slide-up script 
+        $('.scroll-up-button').click(function()
+        {
+            $('html').animate({scrollTop: 0});
+        });
+
+
         //toggle menu/navbar script
         $('.menu-btn').click(function(){
             $('.navbar .menu').toggleClass("active");
             $('.menu-btn i').toggleClass("active");
         });
 
+        //typing animation script
+        var typed = new Typed(".typing", {
+            strings : ["Web Developer" , "Vlogger" , "Coacher" , "Freelancer"],
+            typeSpeed:100,
+            backSpeed:60,
+            loop:true
+        });
+
+        var typed = new Typed(".typing-2", {
+            strings : ["Developer" , "Vlogger" , "Coacher" , "Freelancer"],
+            typeSpeed:100,
+            backSpeed:60,
+            loop:true
+        });
+
+
         // owl-carousel script 
         $('.carousel').owlCarousel({
-            
+            margin: 20,
+            loop: true,
+            autoplayTimeOut:2000,
+            autoplayHoverPause :true,
+            responsive: {
+                0:{
+                    items :1 ,
+                    nav:false
+                },
+                600:
+                {
+                    items :2,
+                    nav:false
+                },
+                1000:{
+                    items :3 ,
+                    nav :false 
+                }
+            }
         });
 });

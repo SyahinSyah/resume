c35feb39443cfae7c71acb6d74e3a54a3342fd40
Syahin Syah <syahinsyah7@gmail.com>
2021-01-15 22:10:30 +0800

    add javascript

--- script.js
+++ script.js
@@ -14,4 +14,9 @@ $(document).ready(function(){
             $('.navbar .menu').toggleClass("active");
             $('.menu-btn i').toggleClass("active");
         });
+
+        // owl-carousel script 
+        $('.carousel').owlCarousel({
+            
+        });
 });
